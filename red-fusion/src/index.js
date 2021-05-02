d7c88f18d360efc4f4eb24997f18ab0425bf7279
Gr1mR34per <vlad.savescu@redbaronsecurity.com>
2021-05-02 22:10:38 +0300

    Added logogs and first stepts for the login system

--- red-fusion/src/index.js
+++ red-fusion/src/index.js
@@ -9,8 +9,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 1000,
+    // Setting the icon 
+    icon: __dirname + '/assets/images/icons/fusion-icon.png',
+    webPreferences: {
+      nodeIntegration: true,
+    }
   });
 
   // and load the index.html of the app.
